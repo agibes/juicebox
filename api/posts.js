a1ff59f56884bd0118f4004733e651af16fd3df7
Anna Gibes <gibesav@gmail.com>
2023-04-26 14:38:53 -0500

    deployment ready

--- api/posts.js
+++ api/posts.js
@@ -3,12 +3,6 @@ const { getAllPosts, createPost, updatePost, getPostById } = require('../db');
 const postsRouter = express.Router();
 const {requireUser} = require('./utils');
 
-// postsRouter.use((req, res, next) => {
-//     console.log('something about a request from users');
-
-//     next();
-// });
-
 postsRouter.post('/', requireUser, async(req, res, next) => {
 //    res.send({message: 'under construction'});
 
@@ -41,8 +35,13 @@ postsRouter.post('/', requireUser, async(req, res, next) => {
 
 postsRouter.get('/', async (req, res) => {
     try { 
-        const posts = await getAllPosts();
+        const allPosts = await getAllPosts();
     
+        //filter out inactive posts
+        const posts = allPosts.filter(post => {
+            return (post.active) || (req.user && post.author.id === req.user.id);
+        });
+
         res.send({
           posts
         });
@@ -52,20 +51,21 @@ postsRouter.get('/', async (req, res) => {
     };
 });
 
+
 postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
     const {postId} = req.params;
     const {title, content, tags } = req.body;
     //set update fields
     const updateFields = {};
-
+    
     if (tags && tags.length > 0) {
         updateFields.tags = tags.trim().split(/\s+/);
     }
-
+    
     if (title) {
         updateFields.title = title;
     }
-
+    
     if (content) {
         updateFields.content = content;
     }
@@ -81,7 +81,7 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
             res.send({post: updatedPost});
         console.log('updated post:', updatedPost);
 
-        } else {
+    } else {
             next({
                 name: 'UnauthorizedUserError', 
                 message: 'You cannot update a post that is not yours'
@@ -92,6 +92,29 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
     }
 });
 
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+    try {
+        const post = await getPostById(req.params.postId);
+        
+        if (post && post.author.id === req.user.id) {
+            const updatedPost = await updatePost(post.id, {active: false})
+            
+            res.send({post: updatedPost});
+        } else {
+            //if there was a post, throw unauthorized user, else throw post not found error
+            next(post ? {
+                name: 'UnauthorizedUserError',
+                message: 'You cannot delete a post which is not yours'
+            } : { 
+                name: 'PostNotFoundError',
+                message: 'That post does not exist'
+            });
+        }
+    } catch ({name, message}) {
+        next({name, message})
+    }
+});
+
 module.exports = postsRouter;
 
 //login albert
@@ -99,3 +122,12 @@ module.exports = postsRouter;
 
 //update post
 //curl http://localhost:3000/api/posts/1 -X PATCH -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhbGJlcnQiLCJwYXNzd29yZCI6ImJlcnRpZTk5IiwiaWF0IjoxNjgyNTMzOTcyfQ.TuOE5APw62d81o5Z-yFrhuu7dg8EGp5yinIYIewkWl8' -H 'Content-Type: application/json' -d '{"title": "updating my old stuff", "tags": "#oldisnewagain"}'
+
+//delete post
+//curl http://localhost:3000/api/posts/1 -X DELETE -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhbGJlcnQiLCJwYXNzd29yZCI6ImJlcnRpZTk5IiwiaWF0IjoxNjgyNTMzOTcyfQ.TuOE5APw62d81o5Z-yFrhuu7dg8EGp5yinIYIewkWl8'
+
+//show all active posts
+//curl http://localhost:3000/api/posts
+
+//show actice + inactive posts for the logged in user
+//curl http://localhost:3000/api/posts -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhbGJlcnQiLCJwYXNzd29yZCI6ImJlcnRpZTk5IiwiaWF0IjoxNjgyNTMzOTcyfQ.TuOE5APw62d81o5Z-yFrhuu7dg8EGp5yinIYIewkWl8'
--- api/tags.js
+++ api/tags.js
@@ -2,12 +2,6 @@ const express = require('express');
 const tagsRouter = express.Router();
 const {getAllTags, getPostsByTagName} = require('../db');
 
-// tagsRouter.use((req, res, next) => {
-//     console.log('something about tags');
-
-//     next();
-// })
-
 tagsRouter.get('/', async (req, res) => {
     try {
         const tags = await getAllTags();
@@ -23,13 +17,22 @@ tagsRouter.get('/', async (req, res) => {
 tagsRouter.get('/:tagName/posts', async (req, res, next) => {
     try {
         const {tagName} = req.params;
-        // console.log(tagName);
         const postsWithTagName = await getPostsByTagName(tagName);
-        // console.log(postsWithTagName);
-        res.send(postsWithTagName);
+
+        const activePosts = postsWithTagName.filter(post => {
+            return (post.active) || (req.user && post.author.id === req.user.id);
+        });
+
+        res.send(activePosts);
     } catch ({name, message}) {
-        next({name: 'TagNameNotFound', message: 'something went wrong'});
+        next({name: 'TagNameError', message: 'something went wrong'});
     }
 });
+
+// does not show inactive posts (with specified tagName) if user is not logged in
+// curl http://localhost:3000/api/tags/%23oldisnewagain/posts
+
+//shows inactive posts (with specified tagName) if user is logged in
+// curl http://localhost:3000/api/tags/%23oldisnewagain/posts -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhbGJlcnQiLCJwYXNzd29yZCI6ImJlcnRpZTk5IiwiaWF0IjoxNjgyNTMzOTcyfQ.TuOE5APw62d81o5Z-yFrhuu7dg8EGp5yinIYIewkWl8'
 
 module.exports = tagsRouter;
